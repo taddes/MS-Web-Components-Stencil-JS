0b7edf1c4c2d79a8924c3f807f96067c5feec339
Taddes Korris <taddes.korris@gmail.com>
2019-03-19 21:31:42 -0400

    element styling

--- 3/tooltip.js
+++ 3/tooltip.js
@@ -2,7 +2,11 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
-    this._tooltipText = 'Default Tooltip text'
+    this._tooltipText = 'Default Tooltip text';
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = (
+      '<slot></slot><span> (?)</span>'
+    );
 
   }
 
@@ -10,12 +14,11 @@ class Tooltip extends HTMLElement {
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text')
     } 
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = '(?)'
+    const tooltipIcon = this.shadowRoot.querySelector('span')
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
+    this.shadowRoot.appendChild(tooltipIcon)
     this.style.position = 'relative'
-    this.appendChild(tooltipIcon);
   }
 
   _showTooltip() {
@@ -25,12 +28,12 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer.style.backgroundColor = 'black'
     this._tooltipContainer.style.position = 'absolute';
     this._tooltipContainer.style.zIndex = '10';
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
 
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer)
+    this.shadowRoot.removeChild(this._tooltipContainer)
   }
 }
 
